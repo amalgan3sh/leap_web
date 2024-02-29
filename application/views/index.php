0e7e983617e4716261b321a7514c28dfc6aed6f0
amalgan3sh <amalganesh4u@gmail.com>
2024-02-29 11:58:27 +0530

    Hover text on country images - AG29022024

--- application/views/index.php
+++ application/views/index.php
@@ -349,66 +349,105 @@ In addition to our visa services, we also offer a wide array of travel solutions
             <div class="col-sm-4 col-xs-6 w3gallery-grids">
                 <a href="<?php echo base_url('/assets/images/visa_processing_non_soudi_nationals.png');?>" class="figure">
                     <img src="<?php echo base_url('/assets/images/country_1.png');?>" alt="" title="Documents required"/> 
+					<div class="overlay">
+                        <span class="text">Guinea</span>
+                    </div>
                 </a> 
             </div> 
             <div class="col-sm-4 col-xs-6 w3gallery-grids">
                 <a href="<?php echo base_url('/assets/images/visa_processing_non_soudi_nationals.png');?>" class="figure">
                     <img src="<?php echo base_url('/assets/images/country_2.png');?>" alt="" title="Documents required"/> 
+					<div class="overlay">
+                        <span class="text">India</span>
+                    </div>
                 </a> 
             </div>
             <div class="col-sm-4 col-xs-6 w3gallery-grids">
                 <a href="<?php echo base_url('/assets/images/visa_processing_non_soudi_nationals.png');?>" class="figure">
                     <img src="<?php echo base_url('/assets/images/country_3.png');?>" alt="" title="Documents required"/> 
+					<div class="overlay">
+                        <span class="text">Russia</span>
+                    </div>
                 </a> 
             </div>
             <div class="col-sm-4 col-xs-6 w3gallery-grids">
                 <a href="<?php echo base_url('/assets/images/visa_processing_non_soudi_nationals.png');?>" class="figure">
                     <img src="<?php echo base_url('/assets/images/country_4.png');?>" alt="" title="Documents required"/> 
+					<div class="overlay">
+                        <span class="text">Brazil</span>
+                    </div>
                 </a> 
             </div>
             <div class="col-sm-4 col-xs-6 w3gallery-grids">
                 <a href="<?php echo base_url('/assets/images/visa_processing_non_soudi_nationals.png');?>" class="figure">
                     <img src="<?php echo base_url('/assets/images/country_5.png');?>" alt="" title="Documents required"/> 
+					<div class="overlay">
+                        <span class="text">Thailand</span>
+                    </div>
                 </a> 
             </div> 
             <div class="col-sm-4 col-xs-6 w3gallery-grids">
                 <a href="<?php echo base_url('/assets/images/visa_processing_non_soudi_nationals.png');?>" class="figure">
                     <img src="<?php echo base_url('/assets/images/country_6.png');?>" alt="" title="Documents required"/> 
+					<div class="overlay">
+                        <span class="text">Australia</span>
+                    </div>
                 </a> 
             </div> 
 			<div class="col-sm-4 col-xs-6 w3gallery-grids">
                 <a href="<?php echo base_url('/assets/images/visa_processing_soudi_nationals.png');?>" class="figure">
                     <img src="<?php echo base_url('/assets/images/country_7.png');?>" alt="" title="Documents required"/> 
+					<div class="overlay">
+                        <span class="text">China</span>
+                    </div>
                 </a> 
             </div> 
 			<div class="col-sm-4 col-xs-6 w3gallery-grids">
                 <a href="<?php echo base_url('/assets/images/visa_processing_soudi_nationals.png');?>" class="figure">
                     <img src="<?php echo base_url('/assets/images/country_8.png');?>" alt="" title="Documents required"/> 
+					<div class="overlay">
+                        <span class="text">Japan</span>
+                    </div>
                 </a> 
             </div> 
 			<div class="col-sm-4 col-xs-6 w3gallery-grids">
                 <a href="<?php echo base_url('/assets/images/visa_processing_soudi_nationals.png');?>" class="figure">
                     <img src="<?php echo base_url('/assets/images/country_9.png');?>" alt="" title="Documents required"/> 
+					<div class="overlay">
+                        <span class="text">EU</span>
+                    </div>
                 </a> 
             </div> 
 			<div class="col-sm-4 col-xs-6 w3gallery-grids">
                 <a href="<?php echo base_url('/assets/images/visa_processing_soudi_nationals.png');?>" class="figure">
                     <img src="<?php echo base_url('/assets/images/country_10.png');?>" alt="" title="Documents required"/> 
+					<div class="overlay">
+                        <span class="text">Mexico</span>
+                    </div>
                 </a> 
             </div> 
 			<div class="col-sm-4 col-xs-6 w3gallery-grids">
                 <a href="<?php echo base_url('/assets/images/visa_processing_soudi_nationals.png');?>" class="figure">
                     <img src="<?php echo base_url('/assets/images/country_11.png');?>" alt="" title="Documents required"/> 
+					<div class="overlay">
+                        <span class="text">Canada</span>
+                    </div>
                 </a> 
             </div> 
 			<div class="col-sm-4 col-xs-6 w3gallery-grids">
                 <a href="<?php echo base_url('/assets/images/visa_processing_soudi_nationals.png');?>" class="figure">
                     <img src="<?php echo base_url('/assets/images/country_12.png');?>" alt="" title="Documents required"/> 
+					<div class="overlay">
+                        <span class="text">UK</span>
+                    </div>
                 </a> 
             </div> 
 			<div class="col-sm-4 col-xs-6 w3gallery-grids">
                 <a href="<?php echo base_url('/assets/images/visa_processing_soudi_nationals.png');?>" class="figure">
                     <img src="<?php echo base_url('/assets/images/country_13.png');?>" alt="" title="Documents required"/> 
+					<div class="overlay">
+                        <span class="text">USA</span>
+                    </div>
                 </a> 
             </div> 
             <!-- Additional image sections can be added here -->
@@ -671,6 +710,7 @@ In addition to our visa services, we also offer a wide array of travel solutions
 	<script>
 		$(function(){
 			var gallery = $('.w3gallery-grids a').simpleLightbox({navText:		['&lsaquo;','&rsaquo;']});
+			$('.sl-navigation').remove();
 		});
 	</script>  
 <!-- //gallery plugin -->
