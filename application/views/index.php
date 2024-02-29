895898ebb401d562cee6a933aae969b19cbc5236
Aswin Ramesh <aswinramesh527@gmail.com>
2024-02-29 10:33:12 +0530

    Added Translation

--- application/views/index.php
+++ application/views/index.php
@@ -31,6 +31,28 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 <link href="//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&amp;subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese" rel="stylesheet">
 <!--//web font-->
 
+
+<!-- Translator -->
+
+	<!-- jQuery Is Required -->
+<script src="/path/to/cdn/jquery.slim.min.js"></script>
+
+<!-- Bootstrap -->
+<link rel="stylesheet" href="/path/to/cdn/bootstrap.min.css" />
+<script src="/path/to/cdn/bootstrap.min.js"></script>
+
+<!-- Bootstrap Select -->
+<link rel="stylesheet" href="/path/to/cdn/bootstrap-select.min.css" />
+<script src="/path/to/cdn/bootstrap-select.min.js"></script>
+
+<!-- Flag Icon -->
+<link rel="stylesheet" href="/path/to/cdn/flag-icon.min.css" />
+
+
+<script src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit" type="text/javascript"></script>
+
+<!-- End-->
+
 </head>
 
 <!-- Body -->
@@ -69,6 +91,16 @@ $logo = "assets/images/logo.png";
 								<a href="#" class="social-button twitter"><i class="fa fa-twitter"></i></a> 
 								<a href="#" class="social-button google"><i class="fa fa-google-plus"></i></a>  
 							</div>
+						</div>
+						<div>	
+						<div id="google_translate_element"></div>
+						<select class="selectpicker" data-width="fit" onchange="translateLanguage(this.value);">
+							<option data-content='<span class="flag-icon flag-icon-af"></span> Afrikaans' value="Afrikaans">Afrikaans</option>
+							<option  data-content='<span class="flag-icon flag-icon-al"></span> Albanian' value="Albanian">Albanian</option>
+							<option  data-content='<span class="flag-icon flag-icon-ar"></span> Arabic' value="Arabic">Arabic</option>
+							<option  data-content='<span class="flag-icon flag-icon-am"></span> Armenian' value="Armenian">Armenian</option>
+							<option  data-content='<span class="flag-icon flag-icon-az"></span> Azerbaijani' value="Azerbaijani">Azerbaijani</option>
+						</select>
 						</div>
 					</nav>
 				</div>
@@ -801,6 +833,27 @@ In addition to our visa services, we also offer a wide array of travel solutions
 
 <!--EmailJS End-->
 
+
+<script>
+		function googleTranslateElementInit() {
+  new google.translate.TranslateElement({ pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE, autoDisplay: false }, 'google_translate_element');
+}
+
+function translateLanguage(lang) {
+  googleTranslateElementInit();
+  var $frame = $('.goog-te-menu-frame:first');
+  if (!$frame.size()) {
+    alert("Error: Could not find Google translate frame.");
+    return false;
+  }
+  $frame.contents().find('.goog-te-menu2-item span.text:contains(' + lang + ')').get(0).click();
+  return false;
+}
+
+$(function(){
+  $('.selectpicker').selectpicker();
+});
+	</script>
 
 </body>
 <!-- //Body -->
